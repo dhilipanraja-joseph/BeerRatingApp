c93e3295ea34f4c5f0fda12a55523d920fdddf23
dhilipanraja-joseph <dlipnraj@gmail.com>
2016-09-05 11:57:44 -0700

    add rating

--- server/models/user.js
+++ server/models/user.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-//  beersratings : [{ type: mongoose.Schema.Types.Ob }]
+  beersratings : {type : Number}
 });
 
 userSchema.statics.register = function(userObj, cb) {
